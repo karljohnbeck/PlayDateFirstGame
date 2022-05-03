6346ceec89f4b682ef702ee4e362f320d5cac281
Karl Beck <karljohnbeck@gmail.com>
2022-05-02 23:39:13 -0500

    first draft of game

--- VSCode-PlaydateTemplate-master/source/main.lua
+++ VSCode-PlaydateTemplate-master/source/main.lua
@@ -2,6 +2,7 @@ import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
+import "CoreLibs/crank"
 
 local gfx <const> = playdate.graphics
 
@@ -12,19 +13,34 @@ local playerSpeed = 4
 local gameTimer = nil
 local playTime = 30 * 1000
 
+local coinSprite = nil
+
+local score = 0
+
 local function resetTimer()
 	playTimer = playdate.timer.new(playTime, playTime, 0, playdate.easingFunctions.linear)
 end
 
+local function moveCoin()
+	local randX = math.random(40, 360)
+	local randY = math.random(40, 200)
+	coinSprite:moveTo(randX, randY)
+end
+
 local function initialize ()
+	math.randomseed(playdate.getSecondsSinceEpoch())
 
 	local playerImage = gfx.image.new("image/player")
-
 	playerSprite = gfx.sprite.new(playerImage)
-
 	playerSprite:moveTo(200, 120)
-
+	playerSprite:setCollideRect(0,0,playerSprite:getSize())
 	playerSprite:add()
+
+	local coinImage = gfx.image.new("image/coin")
+	coinSprite = gfx.sprite.new(coinImage)
+	moveCoin()
+	coinSprite:setCollideRect(0, 0, coinSprite:getSize())
+	coinSprite:add()
 
 			-- game background
 	local backgroundImage = gfx.image.new("image/background")
@@ -45,6 +61,8 @@ function playdate.update()
 	if playTimer.value == 0 then
 		if playdate.buttonIsPressed(playdate.kButtonA) then
 			resetTimer()
+			moveCoin()
+			score = 0
 		end
 	else
 		if playdate.buttonIsPressed(playdate.kButtonUp) then
@@ -59,11 +77,19 @@ function playdate.update()
 		if playdate.buttonIsPressed(playdate.kButtonLeft) then
 			playerSprite:moveBy(-playerSpeed, 0)
 		end
+
+		local collisions = coinSprite:overlappingSprites()
+		if #collisions >= 1 then
+			moveCoin()
+			score += 1
+		end
+
 	end
 
 	playdate.timer.updateTimers()
 	gfx.sprite.update()
 
 	gfx.drawText("Time: " .. math.ceil(playTimer.value/1000), 5, 5)
+	gfx.drawText("Score: " .. score, 320, 5)
 end
 
